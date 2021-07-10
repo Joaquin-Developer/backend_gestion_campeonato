85906f1d5bf7c7fa17704adaf52f6360c0e01054
Joaquin-Parrilla <joaquin.p.olivera@gmail.com>
2021-07-10 01:24:34 -0300

    💡UPDATE: change Sql tables struct

--- database_structure/tables.sql
+++ database_structure/tables.sql
@@ -1,4 +1,3 @@
--- create database gestioncopafutbol;
 
 create table Team(
     IdTeam int unsigned auto_increment,
@@ -9,19 +8,18 @@ create table Team(
     primary key(IdTeam)
 );
 
-create table Match(
-    IdMatch int unsigned auto_increment,
-    DateMatch datetime not null,
+create table Game(
+    IdGame int unsigned auto_increment,
+    DateGame datetime not null,
     GoalsTeam1 int unsigned not null,
     GoalsTeam2 int unsigned not null,
-
-    primary key(IdMatch)
+    primary key(IdGame)
 );
 
-create table Group(
-    IdGroup int unsigned auto_increment,
-    NameGroup varchar(10) not null,
-    primary key(IdGroup)
+create table Serie(
+    IdSerie int unsigned auto_increment,
+    NameSerie varchar(10) not null,
+    primary key(IdSerie)
 );
 
 create table FinalRound(
@@ -30,41 +28,42 @@ create table FinalRound(
     primary key(IdFinalRound)
 );
 
-create table Math_Teams(
-    IdMatch int unsigned auto_increment,
-    IdTeam1 int unsigned auto_increment,
-    IdTeam2 int unsigned auto_increment,
-    primary key(IdMatch, IdTeam1, IdTeam2),
-    foreign key (IdMatch) references Match(IdMatch),
-    foreign key (IdTeam1) references Team(IdTeam),
-    foreign key (IdTeam2) references Team(IdTeam)
+create table Game_Teams(
+    IdGame int unsigned,
+    IdTeam1 int unsigned,
+    IdTeam2 int unsigned,
+    primary key(IdGame, IdTeam1, IdTeam2),
+    foreign key(IdGame) references Game(IdGame),
+    foreign key(IdTeam1) references Team(IdTeam),
+    foreign key(IdTeam2) references Team(IdTeam)
 );
 
-create table Group_Math(
-    IdGroup int unsigned auto_increment,
-    IdMatch int unsigned auto_increment,
-    primary key(IdGroup, IdMatch),
-    foreign key (IdMatch) references Match(IdMatch),
-    foreign key (IdGroup) references Group(IdGroup)
+create table Serie_Game(
+    IdSerie int unsigned,
+    IdGame int unsigned,
+    primary key(IdSerie, IdGame),
+    foreign key(IdGame) references Game(IdGame),
+    foreign key(IdSerie) references Serie(IdSerie)
 );
 
-create table Group_Team(
-    IdGroup int unsigned auto_increment,
-    IdTeam int unsigned auto_increment,
-    WonMatches int unsigned not null,
-    LostMatches int unsigned not null,
-    TiedMatches int unsigned not null,
+create table Serie_Team(
+    IdSerie int unsigned,
+    IdTeam int unsigned,
+    WonGames int unsigned not null,
+    LostGames int unsigned not null,
+    TiedGames int unsigned not null,
     PositiveGoals int unsigned not null,
     NegativeGoals int unsigned not null,
     Points int unsigned not null,
-
-    primary key(IdGroup, IdTeam),
-    foreign key (IdGroup) references Group(IdGroup),
-    foreign key (IdTeam) references Team(IdTeam)
+    primary key(IdSerie, IdTeam),
+    foreign key(IdSerie) references Serie(IdSerie),
+    foreign key(IdTeam) references Team(IdTeam)
 );
 
-create table Match_FinalRound(
-    IdFinalRound int unsigned auto_increment,
-    IdMath int unsigned auto_increment,
-
-)
+create table Game_FinalRound(
+    IdFinalRound int unsigned,
+    IdGame int unsigned,
+    primary key(IdFinalRound, IdGame),
+    foreign key(IdFinalRound) references FinalRound(IdFinalRound),
+    foreign key(IdGame) references Game(IdGame)
+);
