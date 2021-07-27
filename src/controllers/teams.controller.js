280b2144ea7a65b0d7051ce770d2f278daaccc40
Joaquin-Parrilla <joaquin.p.olivera@gmail.com>
2021-07-27 00:38:33 -0300

    ADD update controller

--- src/controllers/teams.controller.js
+++ src/controllers/teams.controller.js
@@ -13,5 +13,16 @@ controller.getAll = async (req, res) => {
     }
 }
 
+controller.update = async (req, res) => {
+    const { id, name, coach, web} = req.body
+    try {
+        const result = await Teams.update({ id, name, coach, web })
+        res.status(200).json({ status: true })
+    } catch(error) {
+        res.status(500).json({ status: false, message: "internal server error." })
+    }
+
+}
+
 
 module.exports = controller
--- src/routes/teams.routes.js
+++ src/routes/teams.routes.js
@@ -4,5 +4,6 @@ const controller = require("../controllers/teams.controller")
 
 router.get("/", function(req, res) { res.json({status: true}) })
 router.get("/getAll", controller.getAll)
+router.post("/update", controller.update)
 
 module.exports = router
